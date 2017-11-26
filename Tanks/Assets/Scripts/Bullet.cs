4919a501e4b848cc9f2df33e7aa283abbc3ef3c6
nzhul <dobromirivanov1@gmail.com>
2017-11-26 19:20:28 +0200

    working on player controler, playermotor and shooting.

---
+++ Tanks/Assets/Scripts/Bullet.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Bullet : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+
+    Collider _collider;
+
+    List<ParticleSystem> _allParticles;
+
+    public int _speed = 100;
+
+    public float _lifetime = 5f;
+
+    public ParticleSystem _explosionFx;
+
+    public List<string> _bounceTags;
+
+    public int _bounces = 2;
+
+
+    private void Start()
+    {
+        _allParticles = GetComponentsInChildren<ParticleSystem>().ToList();
+        _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
+        StartCoroutine(SelftDestruct());
+    }
+
+    private IEnumerator SelftDestruct()
+    {
+        yield return new WaitForSeconds(_lifetime);
+        Explode();
+
+    }
+
+    private void Explode()
+    {
+        _collider.enabled = false;
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.Sleep();
+
+        foreach (MeshRenderer m in GetComponentsInChildren<MeshRenderer>())
+        {
+            m.enabled = false;
+        }
+
+        foreach (ParticleSystem ps in _allParticles)
+        {
+            ps.Stop();
+        }
+
+        if (_explosionFx != null)
+        {
+            _explosionFx.transform.parent = null;
+            _explosionFx.Play();
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (_rigidbody.velocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_bounceTags.Contains(collision.gameObject.tag))
+        {
+            if (_bounces <= 0)
+            {
+                Explode();
+            }
+
+            _bounces--;
+        }
+    }
+
+
+
+}
--- Tanks/Assets/Scripts/PlayerShoot.cs
+++ Tanks/Assets/Scripts/PlayerShoot.cs
@@ -1,17 +1,71 @@
-﻿using UnityEngine.Networking;
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
 
 public class PlayerShoot : NetworkBehaviour
 {
+    public Rigidbody _bulletPrefab;
+
+    public Transform _bulletSpawn;
+
+    public int _shotsPerBurst = 2;
+
+    int _shotsLeft;
+
+    bool _isReloading;
+
+    public float _reloadTime = 1f;
+
 
     // Use this for initialization
     void Start()
     {
-
+        _shotsLeft = _shotsPerBurst;
+        _isReloading = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
 
+    public void Shoot()
+    {
+        if (_isReloading || _bulletPrefab == null)
+        {
+            return;
+        }
+
+        Bullet bullet = null;
+        bullet = _bulletPrefab.GetComponent<Bullet>();
+
+        Rigidbody rbody = Instantiate(_bulletPrefab, _bulletSpawn.position, _bulletSpawn.rotation) as Rigidbody;
+
+        if (rbody != null)
+        {
+            rbody.velocity = bullet._speed * _bulletSpawn.transform.forward;
+        }
+
+        _shotsLeft--;
+
+        if (_shotsLeft <= 0)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    private IEnumerator Reload()
+    {
+        _shotsLeft = _shotsPerBurst;
+        _isReloading = true;
+
+        yield return new WaitForSeconds(_reloadTime);
+
+        _isReloading = false;
     }
 }
